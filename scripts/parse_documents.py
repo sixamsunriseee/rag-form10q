b52b88bb6ba72288129f733aac8b40eade2eb6f6
sxm <artem@buturlakin.family>
2025-07-29 12:12:16 +0200

    script to parse pdfs

---
+++ scripts/parse_documents.py
@@ -0,0 +1,21 @@
+import os
+
+from src.parser import MarkdownParser, TextParser, BaseParser
+from config import DOCS_PATH, MARKDOWNS_PATH, TEXTS_PATH
+
+
+def main(parser: BaseParser, output_directory: str, suffix: str):
+    for filename in os.listdir(DOCS_PATH):
+        if not filename.endswith('.pdf'):
+            continue
+
+        contents = parser.parse_to_string(os.path.join(DOCS_PATH, filename))
+        filename = filename.removesuffix('.pdf') + suffix
+
+        with open(os.path.join(output_directory, filename), 'w') as parsed_file:
+            parsed_file.write(contents)
+
+
+if __name__ == '__main__':
+    main(MarkdownParser(), MARKDOWNS_PATH, '.md')
+    main(TextParser(), TEXTS_PATH, '.txt')
