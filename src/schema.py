707f53e5e2f884281582bf104da26a8d2a92b8ee
sxm <artem@buturlakin.family>
2025-08-08 11:19:40 +0200

    added QueryRoute for LLM, removed ChunkWithContext

--- src/schema.py
+++ src/schema.py
@@ -1,18 +1,22 @@
-from typing import override
+from typing import Literal
+
 from pydantic import BaseModel
+
+
+Year = Literal[2022, 2023]
+Quarter = Literal['Q1', 'Q2', 'Q3']
+Company = Literal['AAPL', 'AMZN', 'INTC', 'MSFT', 'NVDA']
 
 
 class Chunk(BaseModel):
     filename: str
-    text: str
+    year: Year
+    quarter: Quarter
+    company: Company
+    content: str
 
-    @property
-    def content_to_embed(self) -> str: return self.text
 
-
-class ChunkWithContext(Chunk):
-    context: str
-
-    @override
-    @property
-    def content_to_embed(self) -> str: return self.context
+class QueryRoute(BaseModel):
+    companies: list[Company]
+    quarters: list[Quarter]
+    years: list[Year]
