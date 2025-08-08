11778cf5edc650d815746aac491ec1eb412a2fe3
sxm <artem@buturlakin.family>
2025-08-08 11:14:37 +0200

    query return chunks and new chunk schema

--- src/vec_database/dense.py
+++ src/vec_database/dense.py
@@ -1,19 +1,19 @@
 import uuid
-from typing import override
+from typing import override, Iterable
 
 from qdrant_client import models
 from qdrant_client.models import PointStruct
 
-from src.schema import Chunk
+from src.schema import Chunk, QueryRoute
 from src.embedding.base import BaseEmbedding
 from src.vec_database.base import BaseDatabase
-from config import DENSE_CONN_STRING
 
 
 class DenseDatabase(BaseDatabase):
-    def __init__(self, dense: BaseEmbedding):
-        super().__init__(DENSE_CONN_STRING)
+    def __init__(self, conn_string: str, dense: BaseEmbedding):
+        super().__init__(conn_string)
         self.dense = dense
+
 
     @override
     async def create_collection(self, collection_name: str):
@@ -25,22 +25,25 @@ class DenseDatabase(BaseDatabase):
             )
         )
 
+
     @override
     async def chunk_to_point(self, chunk: Chunk) -> PointStruct:
         point = PointStruct(
             id=str(uuid.uuid4()),
-            vector=self.dense.embed(chunk.content_to_embed),
+            vector=await self.dense.embed(chunk.content),
             payload=chunk.model_dump()
         )
 
         return point
 
+
     @override
-    async def query(self, collection_name: str, query: str, limit: int):
+    async def query(self, collection_name: str, query: str, limit: int, route: QueryRoute) -> Iterable[Chunk]:
         points = await self.client.query_points(
             collection_name=collection_name,
-            query=self.dense.embed(query),
-            limit=limit
+            query=await self.dense.embed(query),
+            query_filter=await self.get_query_filter(route),
+            limit=limit,
         )
 
-        return (str(point.payload) for point in points.points)
+        return (Chunk(**point.payload) for point in points.points)
