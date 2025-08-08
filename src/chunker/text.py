c3bf2bb01fb4039c46170a449dce5294827ba9ea
sxm <artem@buturlakin.family>
2025-08-08 11:20:26 +0200

    readability change

--- src/chunker/text.py
+++ src/chunker/text.py
@@ -1,12 +1,17 @@
 from typing import override, Iterable
 
 from chonkie import SentenceChunker
+
 from src.chunker.base import BaseChunker
 
 
 class TextChunker(BaseChunker):
     def __init__(self, chunk_size: int, chunk_overlap: int):
-        self.chunker = SentenceChunker(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+        self.chunker = SentenceChunker(
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap
+        )
+
 
     @override
     def split_to_chunks(self, content: str) -> Iterable[str]:
