b7ce1f31bb60ad127546c72e9b4fbe997ddbb071
sxm <artem@buturlakin.family>
2025-08-08 11:13:18 +0200

    added routing function

--- src/llm/base.py
+++ src/llm/base.py
@@ -1,6 +1,15 @@
 from abc import ABC, abstractmethod
+from typing import Iterable
+
+from src.schema import QueryRoute, Chunk
 
 
 class BaseLLM(ABC):
+    def __init__(self, model_name: str):
+        self.model_name = model_name
+
     @abstractmethod
-    async def query(self, instructions: str, query: str) -> str: ...
+    async def get_query_route(self, query: str) -> QueryRoute: ...
+
+    @abstractmethod
+    async def query(self, instructions: str, chunks: Iterable[Chunk], query: str) -> str: ...
