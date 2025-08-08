5810321f63121b2c8f99ce3a757cf40592b0f9f4
sxm <artem@buturlakin.family>
2025-08-08 11:12:07 +0200

    async + new inference.py

--- src/api.py
+++ src/api.py
@@ -1,12 +1,31 @@
+import json
+
 from fastapi import FastAPI
+
+from src.embedding.fastembed_ import MiniLmEmbedding
 from src.inference import run_inference
+from src.llm.openai_ import OpenLLM
+from src.vec_database.dense import DenseDatabase
+from config import OPENAI_KEY, DENSE_CONN_STRING, BEST_COLLECTION_NAME
 
 app = FastAPI()
+llm = OpenLLM(model_name='gpt-4.1-mini', api_key=OPENAI_KEY)
 
-@app.get("/predict/markdown/")
-async def read_root(query: str):
-    return {"answer": run_inference('markdown-4096-512', query)}
+with open('instructions.json') as file:
+    instructions = str(json.load(file))
 
-@app.get("/predict/text/")
-async def read_root(query: str):
-    return {"answer": run_inference('text-4096-512', query)}
+database = DenseDatabase(conn_string=DENSE_CONN_STRING, dense=MiniLmEmbedding())
+
+
+@app.get("/generate/")
+async def get(query: str):
+    generated, chunks = await run_inference(
+        llm=llm,
+        instructions=instructions,
+        database=database,
+        collection_name=BEST_COLLECTION_NAME,
+        query=query,
+        retrieve_limit=20
+    )
+
+    return {"query": query, "generated": generated}
