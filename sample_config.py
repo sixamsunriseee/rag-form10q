bfdfa937725a278653b9b19d659beeb77239c82e
sxm <artem@buturlakin.family>
2025-08-08 11:19:20 +0200

    initial sample_config

---
+++ sample_config.py
@@ -0,0 +1,10 @@
+DOCS_PATH = 'Path to directory with docs'
+MARKDOWNS_PATH = 'Path to directory with parsed docs as markdown'
+TEXTS_PATH = 'Path to directory with parsed docs as text'
+CHUNKS_PATH = 'Path to directory with json files containing chunks'
+
+OPENAI_KEY = 'YOUR_KEY'
+
+HYBRID_CONN_STRING = 'Connection string for hybrid database'
+DENSE_CONN_STRING = 'Connection string for dense database'
+BEST_COLLECTION_NAME = 'Your best performing collection'
