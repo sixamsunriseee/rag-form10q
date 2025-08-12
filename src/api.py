43c9c49283f528ce92080569e5442295b01f27ee
sxm <artem@buturlakin.family>
2025-08-12 08:47:27 +0200

    changes for new inference

--- src/api.py
+++ src/api.py
@@ -1,31 +1,36 @@
-import json
-
+import uvicorn
 from fastapi import FastAPI
+from fastembed import SparseTextEmbedding, LateInteractionTextEmbedding
 
 from src.embedding.fastembed_ import MiniLmEmbedding
 from src.inference import run_inference
 from src.llm.openai_ import OpenLLM
-from src.vec_database.dense import DenseDatabase
-from config import OPENAI_KEY, DENSE_CONN_STRING, BEST_COLLECTION_NAME
+from src.vec_database.hybrid import HybridDatabase
+from config import OPENAI_KEY, HYBRID_CONN_STRING, BEST_COLLECTION_NAME
 
 app = FastAPI()
-llm = OpenLLM(model_name='gpt-4.1-mini', api_key=OPENAI_KEY)
 
-with open('instructions.json') as file:
-    instructions = str(json.load(file))
+database = HybridDatabase(
+    HYBRID_CONN_STRING,
+    MiniLmEmbedding(),
+    SparseTextEmbedding("Qdrant/bm25"),
+    LateInteractionTextEmbedding("colbert-ir/colbertv2.0")
+)
 
-database = DenseDatabase(conn_string=DENSE_CONN_STRING, dense=MiniLmEmbedding())
-
+llm = OpenLLM(model_name='gpt-4.1-nano', api_key=OPENAI_KEY)
 
 @app.get("/generate/")
-async def get(query: str):
-    generated, chunks = await run_inference(
-        llm=llm,
-        instructions=instructions,
+async def generate(query: str):
+    generated = await run_inference(
         database=database,
         collection_name=BEST_COLLECTION_NAME,
-        query=query,
-        retrieve_limit=20
+        retrieve_limit=5,
+        llm=llm,
+        query=query
     )
 
     return {"query": query, "generated": generated}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='localhost', port=8000)
