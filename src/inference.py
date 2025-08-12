9269c1dde372afd1be9f0a232e83bdfebf091fe9
sxm <artem@buturlakin.family>
2025-08-12 08:30:25 +0200

    cleanup

--- src/inference.py
+++ src/inference.py
@@ -1,23 +1,20 @@
 from src.llm.base import BaseLLM
-from src.schema import Chunk
+from src.schema import QueryChunks
 from src.vec_database.base import BaseDatabase
 
 
-async def run_inference(
-    llm: BaseLLM,
-    instructions: str,
-    database: BaseDatabase,
-    collection_name: str,
-    query: str,
-    retrieve_limit: int
-) -> tuple[str, list[Chunk]]:
-    route = await llm.get_query_route(query)
-    chunks = list(await database.query(collection_name, query, limit=retrieve_limit, route=route))
+async def run_inference(database: BaseDatabase, collection_name: str, retrieve_limit: int, llm: BaseLLM, query: str) -> str:
+    decomposed_queries = await llm.get_subqueries(query)
 
-    response = await llm.query(
-        instructions=instructions,
-        chunks=chunks,
-        query=query,
-    )
+    query_chunks = []
 
-    return response, chunks
+    for decomposed_query in decomposed_queries:
+        route = await llm.get_route(decomposed_query)
+        chunks = await database.get_ordered_chunks(collection_name, decomposed_query, route, retrieve_limit)
+        query_chunks.append(QueryChunks(query=decomposed_query, chunks=chunks))
+
+    answer = await llm.get_answer_multiple(initial_query=query, query_chunks=query_chunks)
+
+    print(answer + '\n')
+
+    return answer
